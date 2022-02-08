62409916266c38a4fb10c8c7138dd554342e632e
prodevweb21 <prodevweb21@gmail.com>
2022-02-07 20:34:48 -0500

    Update index.php

--- api-forfaits-voyages/api/index.php
+++ api-forfaits-voyages/api/index.php
@@ -4,6 +4,10 @@ include_once '../include/fonctions.php';
 
 header('Content-Type: application/json;');
 header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Methods: POST, DELETE, PUT, OPTIONS");
+header('Access-Control-Allow-Headers: Content-Type');
+
+
 
 $mysqli = new mysqli($host, $username, $password, $database);
 if ($mysqli->connect_errno) {
